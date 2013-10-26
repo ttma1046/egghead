32439ff7b9656fc6ff9656297f6ae8646f91553b
Chandler <chandlerfang@gmail.com>
2013-10-26 18:25:59 +1100

    ngRepeat and Filtering Data

--- js/main.js
+++ js/main.js
@@ -1,23 +1,74 @@
 var myApp = angular.module('myApp', []);
 
-myApp.factory('Whatuwant', function() {
-	return { message: 'Anything you want from a service.'};
+myApp.factory('Avengers', function () {
+  var Avengers = {};
+
+  Avengers.cast = [
+    {
+      name: 'Robert Downey Jr.',
+      character: 'Tony Stark / Iron Man'
+    },
+    {
+      name: 'Chris Evans',
+      character: 'Steve Rogers / Captain America'
+    },
+    {
+      name: 'Mark Ruffalo',
+      character: 'Bruce Banner / The Hulk'
+    },
+    {
+      name: 'Chris Hemsworth',
+      character: 'Thor'
+    },
+    {
+      name: 'Scarlett Johansson',
+      character: 'Natasha Romanoff / Black Widow'
+    },
+    {
+      name: 'Jeremy Renner',
+      character: 'Clint Barton / Hawkeye'
+    },
+    {
+      name: 'Tom Hiddleston',
+      character: 'Loki'
+    },
+    {
+      name: 'Clark Gregg',
+      character: 'Agent Phil Coulson'
+    },
+    {
+      name: 'Cobie Smulders',
+      character: 'Agent Maria Hill'
+    },
+    {
+      name: 'Stellan Skarsgård',
+      character: 'Selvig'
+    },
+    {
+      name: 'Samuel L. Jackson',
+      character: 'Nick Fury'
+    },
+    {
+      name: 'Gwyneth Paltrow',
+      character: 'Pepper Potts'
+    },
+    {
+      name: 'Paul Bettany',
+      character: 'Jarvis (voice)'
+    },
+    {
+      name: 'Alexis Denisof',
+      character: 'The Other'
+    },
+    {
+      name: 'Tina Benko',
+      character: 'NASA Scientist'
+    }
+  ];
+
+  return Avengers;
 });
 
-myApp.filter('reverse', function (Whatuwant) {
-	return function (text) {
-		return text.split("").reverse().join("") + Whatuwant.message;
-	};
-});
-
-function FirstCtrl($scope, Whatuwant) {
-	$scope.data = Whatuwant;
-}
-
-function SecondCtrl($scope, Whatuwant) {
-	$scope.data = Whatuwant;
-
-	$scope.reversedMessage = function (message) {
-		return message.split("").reverse().join("");
-	}
+function AvengersCtrl($scope, Avengers) {
+	$scope.avengers = Avengers;
 }
