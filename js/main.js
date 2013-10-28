7838abcc2a03ffd6a85800dd702ac22edfe0f999
Chandler <chandlerfang@gmail.com>
2013-10-28 21:36:46 +1100

    Isolate Scope "="

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ app.controller("AppCtrl", function($scope){
 app.directive('drink', function(){
 	return {
 		scope: {
-			flavor:"@"
+			flavor:"="
 		},
-		template: '<div>{{flavor}}</div>'
+		template: '<input type="text" ng-model="flavor">'
 	}
 });
