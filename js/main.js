47d832b300345272b6ea375d288053114edec14b
Chandler <chandlerfang@gmail.com>
2013-10-28 22:33:06 +1100

    My code.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 var app = angular.module('phoneApp', []);
 
 app.controller("AppCtrl", function($scope){
-	$scope.callHome = function(wut) {
-		alert("called home:" + wut)
-	}
-	
-})
+	$scope.leaveVoicemail = function(number, message) {
+		debugger;
+		alert("Number:" + number + "said:" + message);
+	};	
+});
 
 app.directive('phone', function(){
 	return {
+		restrict: "E",
 		scope: {
-			dial:"&"
+			number:"@",
+			network:"=",
+			makeCall:"&"
 		},
 		template: 
-		'<input type="text" ng-model="value">' + 
-		'<div class="button" ng-click="dial({message:value})">Call home!</div>'
-	}
+		'<div class="panel">Number: {{number}} Network:<select ng-model="network" ng-options="net for net in networks"></div>' + 
+		'<input type="text" ng-model="value">' +
+		'<div class="button" ng-clck="makeCall({number: number, message:value})">Call</div>',
+		link: function (scope) {
+			scope.networks = ["Veroizon", "AT&T", "Sprint"];
+			scope.network = scope.networks[0];
+		}
+	};
 });
