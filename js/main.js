0c968e7f24e43b05cb4585f15bdce1ebfedb1c5b
Chandler <chandlerfang@gmail.com>
2013-12-01 21:04:57 +1100

    ng-view

--- js/main.js
+++ js/main.js
@@ -1,23 +1,16 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
 
-app.run(function($templateCache){
-	$templateCache.put("zippy.html", '<div><h3 ng-click="toggleContent()">{{title}}</h3><div ng-show="isContentVisible" ng-transclude></div></div>');
-});
-
-app.directive("zippy", function ($templateCache) {
-	return {
-		restrict: "E",
-		transclude:true,
-		scope:{
-			title:"@"
-		},
-		templateUrl: 'zippy.html',
-		link: function(scope) {
-			scope.isContentVisible = false;
-
-			scope.toggleContent = function() {
-				scope.isContentVisible = !scope.isContentVisible;
-			} 
+app.config(function ($routeProvider){
+	$routeProvider.when('/', 
+		{
+			templateUrl: "app.html",
+			controller: "AppCtrl"
 		}
+	)
+})
+
+app.controller("AppCtrl", function($scope) {
+	$scope.model = {
+		message: "This is my app!!!"
 	}
-});
+})
